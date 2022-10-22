e3042320bb20320e0f36e29a6a68c457f8e97a50
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-10-23 01:09:57 +0530

    Update solution.py

--- no-idea/solution.py
+++ no-idea/solution.py
@@ -1,1 +1,15 @@
-
+l = list(input().split())
+n,m = l[0],l[1]
+l=[]
+l=list(map(int,input().split()))
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+h=0
+for i in l:
+    if i in A:
+        h+=1
+    elif i in B:
+        h-=1
+    else:
+        pass
+print(h)
