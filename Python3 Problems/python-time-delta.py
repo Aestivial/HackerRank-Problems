595a6b7e8c9503d28841688dd3c517f49d78d8e5
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-11-11 17:25:11 +0530

    Create python-time-delta.py

---
+++ Python3 Problems/python-time-delta.py
@@ -0,0 +1,75 @@
+"""When users post an update on social media,such as a URL, image, status update etc., other users in their network are able to view this new post on their news feed. Users can also see exactly when the post was published, i.e, how many hours, minutes or seconds ago.
+
+Since sometimes posts are published and viewed in different time zones, this can be confusing. You are given two timestamps of one such post that a user can see on his newsfeed in the following format:
+
+Day dd Mon yyyy hh:mm:ss +xxxx
+
+Here +xxxx represents the time zone. Your task is to print the absolute difference (in seconds) between them.
+
+Input Format
+
+The first line contains , the number of testcases.
+Each testcase contains  lines, representing time  and time .
+
+Constraints
+
+Input contains only valid timestamps
+.
+Output Format
+
+Print the absolute difference  in seconds.
+
+Sample Input 0
+
+2
+Sun 10 May 2015 13:54:36 -0700
+Sun 10 May 2015 13:54:36 -0000
+Sat 02 May 2015 19:54:36 +0530
+Fri 01 May 2015 13:54:36 -0000
+Sample Output 0
+
+25200
+88200
+Explanation 0
+
+In the first query, when we compare the time in UTC for both the time stamps, we see a difference of 7 hours. which is  seconds or  seconds.
+
+Similarly, in the second query, time difference is 5 hours and 30 minutes for time zone adjusting for that we have a difference of 1 day and 30 minutes. Or """
+
+
+#!/bin/python3
+from datetime import datetime, timedelta
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    format_date = "%a %d %b %Y %H:%M:%S %z"
+    t1=datetime.strptime(t1, format_date)
+    t2=datetime.strptime(t2, format_date)
+    return str(int(abs((t1-t2).total_seconds())))
+    datetime.total_seconds()
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
+
+
+
+
+
+
