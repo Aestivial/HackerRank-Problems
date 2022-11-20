7abae811d85b68aabf7cd787bbd7c5b98c2e3468
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-11-21 01:31:26 +0530

    Update dynamic-array.py

--- Problem  Solving/dynamic-array.py
+++ Problem  Solving/dynamic-array.py
@@ -1,3 +1,87 @@
+'''
+Declare a 2-dimensional array, , of  empty arrays. All arrays are zero indexed.
+Declare an integer, , and initialize it to .
+
+There are  types of queries, given as an array of strings for you to parse:
+
+Query: 1 x y
+Let .
+Append the integer  to .
+Query: 2 x y
+Let .
+Assign the value  to .
+Store the new value of  to an answers array.
+Note:  is the bitwise XOR operation, which corresponds to the ^ operator in most languages. Learn more about it on Wikipedia.  is the modulo operator.
+Finally, size(arr[idx]) is the number of elements in arr[idx]
+
+Function Description
+
+Complete the dynamicArray function below.
+
+dynamicArray has the following parameters:
+- int n: the number of empty arrays to initialize in 
+- string queries[q]: query strings that contain 3 space-separated integers
+
+Returns
+
+int[]: the results of each type 2 query in the order they are presented
+Input Format
+
+The first line contains two space-separated integers, , the size of  to create, and , the number of queries, respectively.
+Each of the  subsequent lines contains a query string, .
+
+Constraints
+
+It is guaranteed that query type  will never query an empty array or index.
+Sample Input
+
+2 5
+1 0 5
+1 1 7
+1 0 3
+2 1 0
+2 1 1
+Sample Output
+
+7
+3
+Explanation
+
+Initial Values:
+
+
+ = [ ]
+ = [ ]
+
+Query 0: Append  to .
+
+ = [5]
+ = [ ]
+
+Query 1: Append  to .
+ = [5]
+ = [7]
+
+Query 2: Append  to .
+
+ = [5, 3]
+ = [7]
+
+Query 3: Assign the value at index  of  to , print .
+
+ = [5, 3]
+ = [7]
+
+7
+Query 4: Assign the value at index  of  to , print .
+
+ = [5, 3]
+ = [7]
+
+3
+'''
+
+
 #!/bin/python3
 
 import math
@@ -5,43 +89,26 @@ import os
 import random
 import re
 import sys
-#
-# Complete the 'dynamicArray' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. 2D_INTEGER_ARRAY queries
-#
 
 def dynamicArray(n, queries):
-    l,a=[],[]
-    la=0
+    lastanswer=0
+    arr=[[] for _ in range(n)]
+    
     for q in queries:
-        idx=(q[1]^la)%n
-        if q[0]==1:
-            l.insert(idx,q[2])
-        elif q[0]==2:
-            la=l[idx][q[2]%len(l[idx])]
-            a.append(la)
-        return []
+        op,x,y=q
+        if op==1:
+            arr[(x^lastanswer)%n].append(y)
+        elif op==2:
+            s=arr[(x^lastanswer)%n]
+            lastanswer=s[y%len(s)]
+            print(lastanswer)
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    first_multiple_input = input().rstrip().split()
-
-    n = int(first_multiple_input[0])
-    q = int(first_multiple_input[1])
-
+    n,q = map(int,input().rstrip().split())
     queries = []
 
     for _ in range(q):
         queries.append(list(map(int, input().rstrip().split())))
 
-    result = dynamicArray(n, queries)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+    dynamicArray(n, queries)
