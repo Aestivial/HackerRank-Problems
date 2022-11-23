036d870f31b3773436d14507365fb49c2c1f09a3
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-11-23 23:26:42 +0530

    Update and rename py-set-discard-remove-add/question.txt to Python3 Programming/py-set-discard-remove-add.py

--- Python3 Programming/py-set-discard-remove-add.py
+++ Python3 Programming/py-set-discard-remove-add.py
@@ -1,4 +1,4 @@
-.remove(x)
+'''.remove(x)
 This operation removes element  from the set.
 If element  does not exist, it raises a KeyError.
 The .remove(x) operation returns None.
@@ -89,3 +89,23 @@ Explanation
 After completing these  operations on the set, we get set. Hence, the sum is .
 
 Note: Convert the elements of set s to integers while you are assigning them. To ensure the proper input of the set, we have added the first two lines of code to the editor.
+'''
+n=int(input())
+s=set(map(int,input().split()))
+
+for i in range(int(input())):
+    inp = input().split()
+    if inp[0]=='remove':
+        if int(inp[1]) in s:
+            s.remove(int(inp[1]))
+        else:
+            pass
+    elif inp[0]=='discard':
+        s.discard(int(inp[1]))
+    elif inp[0]=='pop':
+        if len(s)==0:
+            pass
+        else:
+            s.discard(next(iter(s)))
+            ### POP didn't work as expected lmao
+print(sum(s))
